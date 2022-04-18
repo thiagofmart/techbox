223940b4bb9a1dc75d3e14b9aa0ddf6d81935ecb
Thiago Martins <71234183+thiagofmart@users.noreply.github.com>
2022-04-18 19:03:44 -0300

    Delete backend directory

--- backend/sql_app/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-
-engine = create_engine("sqlite:///./sql_app/test.db", connect_args={"check_same_thread": False})
-Session = sessionmaker(bind=engine)
-session = Session()
-Base = declarative_base()
--- backend/sql_app/tools.py
+++
@@ -1,9 +0,0 @@
-from hashlib import pbkdf2_hmac
-
-SALT = b'MATHEUSSAMUELTHIAGO'*2
-KEY='38333295000'
-SECRET='fed2163e2e8dccb53ff914ce9e2f1258'
-
-
-def encrypt_pass(password):
-    return pbkdf2_hmac('sha256', password.encode(), SALT, 500000).hex()
